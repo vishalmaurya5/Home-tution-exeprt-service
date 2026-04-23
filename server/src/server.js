68c82a23b071ef86db6ad12043bebfc38f45cb11
vishalmaurya5 <vishalverma5359@gmail.com>
2026-04-23 23:07:30 +0530

    u

--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
+/* ne     change*/
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../../dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
+});
+
 
 
 const startServer = async () => {
